782714518b46e2898140429470dd3e554853b9a3
Abraham <abraham.dev@yahoo.com>
2022-02-02 18:32:01 -0600

    Nested forms ready and participating_user relation are ready too

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :owner_tasks
+  has_many :owned_tasks
   # Aquí se pone participations porque es lo que hace y es más descriptivo que poner participant, esto se hace con la intención de que se entienda mejor lo que se programa, pero como el modelo no se llama participations entonces usamos class_name
   has_many :participations, class_name: 'Participant'
   # Contiene muchas tareas y accede a ellas a travez de las participaciones que tiene 
